4e6733c3279b290bfee2c9fd811fb2bfd924dcf8
sdjoao <sdjoaoasl@gmail.com>
2026-01-17 09:07:28 -0300

    feat(user): repositorio de usuario para implementar metodos do jakarta persistence api.

---
+++ src/main/java/br/com/atos/app/repository/user/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.atos.app.repository.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.atos.app.model.user.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
