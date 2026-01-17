0b548c47fc06ab48b43313acf667e2fffa67234c
sdjoao <sdjoaoasl@gmail.com>
2026-01-17 09:03:01 -0300

    feat(users): modelacao da entidade usuario.

---
+++ src/main/java/br/com/atos/app/model/user/User.java
@@ -0,0 +1,39 @@
+package br.com.atos.app.model.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String password;
+
+    public User(){}
+
+    public User(String name, String password){
+        this.name = name;
+        this.password = password;
+    }
+
+    public Long getId(){ return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName(){ return name; }
+    public void setName(String name){ this.name = name; }
+
+    public String getPassword(){ return password; }
+    public void setPassword(String password){ this.password = password; }
+}
