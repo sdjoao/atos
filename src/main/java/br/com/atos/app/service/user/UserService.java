a327299705659f0d3ac1a6793e6fb7cedcdbe5f9
sdjoao <sdjoaoasl@gmail.com>
2026-01-31 10:35:47 -0300

    feat(user): serviço para atualizar cadastro de usuario.

--- src/main/java/br/com/atos/app/service/user/UserService.java
+++ src/main/java/br/com/atos/app/service/user/UserService.java
@@ -21,6 +21,11 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    private String encryptPassword(String password){
+        String hash = encoder.encode(password);
+        return hash;
+    }
+
     public UserResponseDTO criarUsuario(UserRequestDTO usuario){
         if(userRepository.existsByName(usuario.name())){
             throw new IllegalArgumentException("Usuário já existe.");
@@ -28,7 +33,7 @@ public class UserService {
 
         User novoUsuario = new User();
         novoUsuario.setName(usuario.name());
-        novoUsuario.setPassword(encoder.encode(usuario.password()));
+        novoUsuario.setPassword(encryptPassword(usuario.password()));
 
         User usuarioSalvo = userRepository.save(novoUsuario);
 
@@ -61,5 +66,18 @@ public class UserService {
             usuarioEncontrado.getName()
         );
         return usuario;
+    }
+
+    public UserResponseDTO atualizarUsuario(Long id, UserRequestDTO user){
+        User usuarioEncontrado = userRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        usuarioEncontrado.setName(user.name());
+        usuarioEncontrado.setPassword(encryptPassword(user.password()));
+        userRepository.save(usuarioEncontrado);
+        UserResponseDTO usuarioAtualizado = new UserResponseDTO(
+            usuarioEncontrado.getId(),
+            usuarioEncontrado.getName()
+        );
+        return usuarioAtualizado;
     }
 }
